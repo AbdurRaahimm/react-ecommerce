8ee2d17a544320dcc5c6347b56a58380e0dfdafc
“AbdurRaahimm” <rahim703936@gmail.com>
2024-10-04 18:59:13 +0600

    modify search bar

--- src/components/FilterProducts.jsx
+++ src/components/FilterProducts.jsx
@@ -7,9 +7,21 @@ export default function FilterProducts({ state, products, setState }) {
             {/* search product */}
             <div className="space-y-2">
                 <h6 className="text-sky-600 font-bold text-xl">Search Products</h6>
-                <input
-                    onChange={(e) => setState({ ...state, search: e.target.value })}
-                    type="search" placeholder="Search Products" className="w-full p-2 rounded-md focus:outline-none" />
+                <label className="relative block">
+                    <span className="sr-only">Search</span>
+                    <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+                        </svg>
+                    </span>
+                    <input
+                        onChange={(e) => setState({ ...state, search: e.target.value })}
+                        className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+                        placeholder="Search Products..."
+                        type="search"
+                        name="search" />
+                </label>
+
             </div>
 
             {/* categories */}
