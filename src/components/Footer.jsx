0b716479821f36eb776791f2de4da7f04a2064ed
“AbdurRaahimm” <rahim703936@gmail.com>
2024-10-05 14:07:27 +0600

    modify footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,10 @@ export default function Footer() {
         <footer className="">
             <FooterTop />
             <hr />
-            <p className='text-center py-2'>&copy; 2023-{new Date().getFullYear()} ShopEasy. All rights reserved.</p>
+            <p className='text-center py-2'>&copy; 2023-{new Date().getFullYear()} 
+            ShopEasy. All rights reserved. Develop By 
+            <a href="https://fb.com/Rahim72446" target="_blank" rel="noreferrer" className='text-[#0ea5e9]'> Abdur Rahim</a> 
+            </p>
         </footer>
     )
 }
